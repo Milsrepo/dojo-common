940e4ce815cec064b0d86bf806f2b94a4ab4d84b
Milotskiy Alexey <spalaxinco@gmail.com>
2013-12-22 00:36:45 +0200

    Added new response handler, for handling Data parameter.

---
+++ response/_DataMixin.js
@@ -0,0 +1,52 @@
+define([
+        "dojo/_base/declare",
+        "./_OptionalMixin",
+        "./_GetterMixin"
+        ], function(declare, _OptionalMixin, _GetterMixin) {
+// module:
+//      dojo-common/component/response/_DataMixin
+
+    var _responseKey = 'data';
+
+    return declare([ _OptionalMixin, _GetterMixin ], {
+        // summary:
+        //      This mixin should be used if response will support
+        //      data
+
+        data: null,
+
+        constructor: function (data, opt) {
+            try {
+                this.data = this.extractData(data || {});
+            } catch (e) {
+                console.error(this.declaredClass+" "+arguments.callee.nom, arguments, e);
+                throw e;
+            }
+        },
+
+        getData: function (fallbackValue) {
+            // summary:
+            //      Return data from data object
+            try {
+                return this.defaultGetter(_responseKey, fallbackValue);
+            } catch (e) {
+                console.error(this.declaredClass+" "+arguments.callee.nom, arguments, e);
+                throw e;
+            }
+        },
+
+
+        extractData: function (data) {
+            // summary:
+            //      Method for extracting data
+            //      from data
+            try {
+               data = data || {};
+               return data[_responseKey] || null;
+            } catch (e) {
+                 console.error(this.declaredClass+" "+arguments.callee.nom, arguments, e);
+                 throw e;
+            }
+        }
+    });
+});
