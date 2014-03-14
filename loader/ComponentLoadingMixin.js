76eba52eca350df219da5203f4eb3e6760e13be5
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2014-03-14 18:21:42 +0200

    Added usefull Mixin to wrap widgets while they content is loading with loader.

---
+++ loader/ComponentLoadingMixin.js
@@ -0,0 +1,54 @@
+define(['dojo/_base/declare',
+        'dijit/_Widget',
+        'dojo/_base/lang',
+        'dojo/query',
+        'dojo/aspect',
+        'dojo/dom-construct',
+        'dojo/NodeList-manipulate',
+        'dojo/NodeList-traverse'],
+    function(declare, _Widget, lang, query, aspect, domConstruct) {
+
+        return declare([ _Widget ], {
+            constructor: function () {
+                try {
+                    aspect.after(this, 'buildRendering',
+                                  lang.hitch(this, '_buildRendering'));
+
+                    aspect.after(this, 'startup',
+                                  lang.hitch(this, '_startup'));
+                } catch (e) {
+                     console.error(this.declaredClass, arguments, e);
+                     throw e;
+                }
+            },
+
+            _buildRendering: function () {
+                try {
+                    this.contentNode = domConstruct.create('div');
+                    query(this.domNode).children().wrapAll(this.contentNode);
+
+                    this.loadingNode = domConstruct.create('div',
+                                                           {'class': 'loading',
+                                                            'innerHTML': ''},
+                                                           this.domNode, 'first');
+
+                    query(this.contentNode).style('display', 'none');
+                    query(this.loadingNode).style('display', '');
+                } catch (e) {
+                     console.error(this.declaredClass, arguments, e);
+                     throw e;
+                }
+            },
+
+            _startup: function () {
+                try {
+                    query(this.contentNode).children().place(this.domNode);
+                    this.contentNode && query(this.contentNode).orphan();
+                    this.loadingNode && query(this.loadingNode).orphan();
+                } catch (e) {
+                     console.error(this.declaredClass, arguments, e);
+                     throw e;
+                }
+            }
+        });
+    });
